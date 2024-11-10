50680d8c0d33a3fde0cfcefb5ede267c5cba8b4c
Petra <144622676+knastman@users.noreply.github.com>
2024-11-10 22:18:57 +0100

    Win/Lose message added
    Beroende på status i varje cell på hela brädet, skrivs det ut meddelande när man har vunnit eller förlorat. (Det gör även cellerna oklickbara efter att man vunnit el förlorat).

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,23 +7,49 @@ class Board extends React.Component {
     super(props);
 
     const cells = createBoard(this.props.boardSize, this.props.numberOfMines);
+    console.log(cells);
+    
 
     this.state = {
       cells: cells,
+      gameStatus: "playing", 
     };
   }
 
   handleClick = (index) => {
-    const { cells } = this.state;
-    if (cells[index].visible) return;
-  
-    const updatedCells = [...cells];
-    updatedCells[index] = { ...updatedCells[index], visible: true };
-    this.setState({ cells: updatedCells });
+    const { cells, gameStatus } = this.state; 
+    if (cells[index].visible || gameStatus !== 'playing') return; 
+    
+    const updatedCells = [...cells]; 
+
+    updatedCells[index] = { 
+      ...updatedCells[index], 
+      visible: true
+    };
+
+    let newGameStatus = "playing";
+    if (updatedCells[index].hasMine) {
+      newGameStatus = "gameOver";
+    }
+    else {
+      newGameStatus = "gameWon";
+      
+      for (const cell of updatedCells) {
+        if (!cell.visible && !cell.hasMine) {
+          newGameStatus = "playing";
+          break;
+        }
+      }
+    }
+
+    this.setState({ 
+      cells: updatedCells,
+      gameStatus: newGameStatus
+    });
   };
 
   render() {
-    
+    const { gameStatus } = this.state; 
 
     return (
       <div className="wrapper">
@@ -35,7 +61,8 @@ class Board extends React.Component {
         </div> 
 
         <div className='winLoseInfo'>
-
+          {gameStatus === "gameOver" && <p>Game Over!</p>}
+          {gameStatus === "gameWon" && <p>Game Won!</p>}
         </div>
 
       </div>
